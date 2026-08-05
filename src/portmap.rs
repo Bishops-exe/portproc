1ea9f079b0406b7cb941acc4d938dfc4ba8fdcb3
Bishops-exe <Bishops-exe@users.noreply.github.com>
2026-08-05 18:17:20 +0200

    feat: add 17 more port presets

--- src/portmap.rs
+++ src/portmap.rs
@@ -21,7 +21,24 @@ static PRESETS: phf::Map<&'static str, &'static str> = phf_map! {
     "rdp" => "T3389",
     "mysql" => "T3306",
     "ms-sql" => "T1433",
-    "mongodb" => "T27017"
+    "mongodb" => "T27017",
+    "http" => "T80",
+    "https" => "T443",
+    "redis" => "T6379",
+    "smtp" => "T25",
+    "smtps" => "T465",
+    "imap" => "T143",
+    "imaps" => "T993",
+    "pop3" => "T110",
+    "pop3s" => "T995",
+    "telnet" => "T23",
+    "memcached" => "T11211",
+    "elasticsearch" => "T9200",
+    "rabbitmq" => "T5672",
+    "k8s-api" => "T6443",
+    "next-dev" => "T3000",
+    "cra-dev" => reference!("next-dev"),
+    "create-react-app-dev" => reference!("cra-dev")
 };
 
 pub fn preset(value: &str) -> Result<&str, Errors> {
